30f0d5cac30292d799aaa588594cd41563c93103
David <idavid@fastmail.com>
2020-04-15 09:34:24 -0600

    new mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,35 +1,39 @@
 export const setProducts = (state, payload) => state.products = payload
 
 export const appendToCart = (state, {product, quantity}) =>  {
-    let productExists = findExistingProduct(state, product)
+  let productExists = findExistingProduct(state, product)
 
-    if (productExists){
-        productExists.quantity++
-    } else {
-        state.cart.push({
-            product,
-            quantity
-        })
-    }
+  if (productExists){
+    productExists.quantity++
+  } else {
+    state.cart.push({
+      product,
+      quantity
+    })
+  }
 }
+
+export const setCartItems = (state, payload) => state.cart = payload
 
 export const removeFromCart = (state, {product,quantity}) => {
-   let productExists = findExistingProduct(state, product)
+  let productExists = findExistingProduct(state, product)
 
-   if (quantity > 1) {
-       productExists.quantity-- //if product exists decrease the quantity one by one e.g. 3, 2, 1
-   } else {
-       // remove item from state (cart)
-       state.cart.find((item, index) => {
-           if(item.product.id === product.id) {
-               state.cart.splice(index, 1)
-           }
-       })
-   }
+  if (quantity > 1) {
+    productExists.quantity-- //if product exists decrease the quantity one by one e.g. 3, 2, 1
+  } else {
+    // remove item from state (cart)
+    state.cart.find((item, index) => {
+      if(item.product.id === product.id) {
+        state.cart.splice(index, 1)
+      }
+    })
+  }
 }
 
+export const removeAllItems = (state) => state.cart = []
+
 function findExistingProduct(state, product) {
-    return state.cart.find(item => {
-        return item.product.id === product.id
-    })
+  return state.cart.find(item => {
+    return item.product.id === product.id
+  })
 }
