fec58b0970b213ffe82338523404be1200f859a8
David <idavid@fastmail.com>
2020-04-15 09:34:32 -0600

    cart count / total

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,3 +2,9 @@ export const products = state => state.products
 
 // cart
 export const cart = state => state.cart
+
+export const cartCount = state => state.cart.length
+
+export const cartTotal = state => state.cart.reduce((previous, current)=> {
+  return previous + current.product.price * current.quantity
+}, 0).toFixed(2)
